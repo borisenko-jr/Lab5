07dd5220673b1e3927f602e7aec93397647f5b4e
borisenko-jr <43934831+borisenko-jr@users.noreply.github.com>
2018-11-22 18:17:10 +0300

    Hi

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace GetUserName
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGetUserName = new System.Windows.Forms.Label();
+            this.btnGetUserName = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblGetUserName
+            // 
+            this.lblGetUserName.AutoSize = true;
+            this.lblGetUserName.Location = new System.Drawing.Point(168, 20);
+            this.lblGetUserName.Name = "lblGetUserName";
+            this.lblGetUserName.Size = new System.Drawing.Size(0, 13);
+            this.lblGetUserName.TabIndex = 0;
+            // 
+            // btnGetUserName
+            // 
+            this.btnGetUserName.Location = new System.Drawing.Point(16, 20);
+            this.btnGetUserName.Name = "btnGetUserName";
+            this.btnGetUserName.Size = new System.Drawing.Size(75, 23);
+            this.btnGetUserName.TabIndex = 1;
+            this.btnGetUserName.Text = "Получить!";
+            this.btnGetUserName.UseVisualStyleBackColor = true;
+            this.btnGetUserName.Click += new System.EventHandler(this.btnGetUserName_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 57);
+            this.Controls.Add(this.btnGetUserName);
+            this.Controls.Add(this.lblGetUserName);
+            this.Name = "Form1";
+            this.Text = "Получение имени пользователя";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGetUserName;
+        private System.Windows.Forms.Button btnGetUserName;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetUserName
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetUserName_Click(object sender, EventArgs e)
+        {
+            StringBuilder builder = new StringBuilder(100);
+            uint size = 100;
+            WinAPIClass.GetUserNameEx((int)WinAPIClass.NameFormat.NameSamCompatible, builder, ref size);
+            lblGetUserName.Text = String.Format("Имя пользователя: {0}", builder.ToString());
+        }
+    }
+}
